cd6ccfbe4c9798518f5d68c4dccc8b73c1c12cb7
pjo901018 <pjo901018@korea.ac.kr>
2018-01-09 11:07:54 +0900

    세임에게 질문받았던 문제

---
+++ python_questionfromfriend/script.py
@@ -0,0 +1,36 @@
+#
+# case_num = int(input())
+# input_strings = list()
+#
+# for case_idx in range(case_num):
+#     input_strings.append(input())
+#
+# for each in input_strings:
+#     find_num(each)
+#
+# def find_num(input_string):
+#     percentage = 0
+#
+#     return percentage
+#
+# answer = []
+#
+# for k in range(case_num):
+#     input_string = input()
+#     input_splited = input_string.split()
+#     input_mapped = map(int, input_splited)
+#     input_list = list(input_mapped)
+#     student_num = input_list[0]
+#     line_avg = sum(input_list[1:]) / student_num
+#     upper_list = []
+#     upper_num = 0
+#     for i in input_list:
+#         if i > line_avg:
+#             upper_num += 1
+#     answer.append("%0.3f" % round(upper_num / student_num * 100, 3))
+# for i in answer:
+#     print("%s%%" % i)
+
+print(1.0 + 1.6 + 5.1)
+print(1.6 + 5.1)
+print(25.6 + 5.1)
